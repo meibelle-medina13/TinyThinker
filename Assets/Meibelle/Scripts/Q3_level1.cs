b104813b64f755653a27fdc951ac677413ddc090
Meibelle Medina <113251217+meibelle-medina13@users.noreply.github.com>
2025-03-03 23:25:35 +0800

    additional update

--- Assets/Meibelle/Scripts/Q3_level1.cs
+++ Assets/Meibelle/Scripts/Q3_level1.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
-using UnityEditor.PackageManager;
 using System;
 using UnityEngine.TextCore.Text;
 using UnityEngine.Playables;
